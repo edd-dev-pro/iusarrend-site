cabe01db0479b3ef8f1742bb8699550e3760bb70
edd.dev <soka_117@live.com>
2025-06-24 19:58:05 -0600

    feat: the nav link component is created

---
+++ src/core/navLink/NavLink.tsx
@@ -0,0 +1,22 @@
+import { NavLink as RouterLink, type NavLinkProps } from "react-router-dom";
+import type { ReactNode } from "react";
+import classNames from "classnames";
+
+interface Props extends NavLinkProps {
+  children: ReactNode;
+}
+
+const NavLink = ({ children, className, ...props }: Props) => {
+  return (
+    <RouterLink
+      {...props}
+      className={({ isActive }) =>
+        classNames("nav-link", className, { active: isActive })
+      }
+    >
+      {children}
+    </RouterLink>
+  );
+};
+
+export default NavLink;
