7b4396c1d9f1507069d9b0509e5ef6e470cfed8e
edd.dev <soka_117@live.com>
2025-06-24 19:49:17 -0600

    feat: the header component is updated

--- src/core/header/Header.tsx
+++ src/core/header/Header.tsx
@@ -1,5 +1,24 @@
+import NavLink from "../navLink/NavLink";
+import { Container, Navbar, Nav } from "react-bootstrap";
+
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="py-3">
+      <Container>
+        <Navbar expand="lg" className="justify-content-between">
+          <Navbar.Brand href="/">IUSARREND</Navbar.Brand>
+          <Nav>
+            <NavLink to="/">Inicio</NavLink>
+            <NavLink to="/services">Servicios</NavLink>
+            <NavLink to="/blog">Blog</NavLink>
+            <NavLink to="/lawyers">Abogados</NavLink>
+            <NavLink to="/cases">Casos</NavLink>
+            <NavLink to="/contact">Contacto</NavLink>
+          </Nav>
+        </Navbar>
+      </Container>
+    </header>
+  );
 };
 
 export default Header;
