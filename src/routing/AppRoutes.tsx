88ddecd130978de3b5310c0e1b326a03d927cfcd
edd.dev <soka_117@live.com>
2025-06-24 19:56:29 -0600

    feat: pages are updated in app routes

--- src/routing/AppRoutes.tsx
+++ src/routing/AppRoutes.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import { Home } from "../pages";
+import { Home, PlansPrices, TermsConditions, Contact } from "../pages";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/" element={<PlansPrices />} />
+      <Route path="/" element={<TermsConditions />} />
+      <Route path="/" element={<Contact />} />
     </Routes>
   );
 };
