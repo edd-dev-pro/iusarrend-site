6e8f7cf66a98ea47639afbd76f24f016ce8d747b
edd.dev <soka_117@live.com>
2025-06-26 10:59:03 -0600

    feat: the content and styles of the main section are updated

--- src/sections/mainSection/MainSection.tsx
+++ src/sections/mainSection/MainSection.tsx
@@ -1,5 +1,24 @@
+import clsx from 'clsx'
+import styles from './styles/mainSection.module.css'
+
 const MainSection = () => {
-  return <div>MainSection</div>
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center">
+      <h1 className={styles.title}>Renta Segura</h1>
+      <p className={clsx('text-light', styles.subTitle)}>
+        Lorem ipsum dolor sit amet
+      </p>
+      <button
+        className={clsx(
+          'btn text-light border border-2 rounded-pill mt-4 px-4',
+          styles.btnMain,
+        )}
+        type="submit"
+      >
+        Agendar
+      </button>
+    </div>
+  )
 }
 
 export default MainSection
