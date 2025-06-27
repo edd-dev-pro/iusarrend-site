aa78f0d60ee2a7e6e16594181569801a1a2c4b3d
edd.dev <soka_117@live.com>
2025-06-26 20:11:47 -0600

    feat: content is added to the termsconditions section

--- src/sections/termsConditionsSection/components/privacyNotice/PrivacyNotice.tsx
+++ src/sections/termsConditionsSection/components/privacyNotice/PrivacyNotice.tsx
@@ -1,5 +1,37 @@
 const PrivacyNotice = () => {
-  return <div>PrivacyNotice</div>
+  return (
+    <div className="limit">
+      <ul>
+        <li>
+          No se podrá continuar con el proceso de póliza de arrendamiento si el
+          prospecto (arrendatario) se encuentra en situación de adeudo
+          hipotecario, automotriz, dentro de un procedimiento familiar, civil o
+          penal.
+        </li>
+        <li>IVA incluido</li>
+        <li>Los costos se encuentran sujetos a cambios sin previo aviso.</li>
+        <li>
+          La investigación de registro público de la propiedad o su similar se
+          realizara en la Ciudad de México o Área Metropolitana, si se encuentra
+          fuera de dichos lugares, consultar costo con su ejecutivo de venta.
+        </li>
+        <li>
+          Para dar inicio con el trámite de contratación se requiere el anticipo
+          de $1,000.00 (un mil pesos 00/100 M.N) mismos que serán tomados a
+          cuenta para el pago total de la póliza, en caso de no concretar la
+          operación, por gastos operativos, NO habrá devolución.
+        </li>
+        <li>
+          Toda la documentación deberá ser enviada al correo:
+          iusarrendclientes@gmail.com
+        </li>
+        <li>
+          Todos los documentos solicitados deben ser con fecha de expedición no
+          mayor a 3 meses
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default PrivacyNotice
