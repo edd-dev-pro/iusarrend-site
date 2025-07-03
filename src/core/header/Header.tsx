d125ee3394bed33bbade17c98cce8d79518b4638
edd.dev <soka_117@live.com>
2025-07-02 21:10:45 -0600

    feat: the menu is integrated into the header

--- src/core/header/Header.tsx
+++ src/core/header/Header.tsx
@@ -1,56 +1,40 @@
-import type { FC } from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap'
-import { Link, useLocation } from 'react-router-dom'
-import NavLinkItem from './components/NavLinkItem'
+import clsx from 'clsx'
+import { Container } from 'react-bootstrap'
 
+import Menu from '../menu/Menu'
+import MenuLink from '../menu/components/menuLink/MenuLink'
 import styles from './styles/header.module.css'
 
-const Header: FC = () => {
-  const location = useLocation()
-
+const Header = () => {
   return (
-    <header className={styles.headerWrapper}>
-      <Navbar expand="lg" className={styles.navbar}>
-        <Container fluid className={styles.container}>
-          <Nav className={styles.leftNav}>
-            <NavLinkItem
-              to="/"
-              label="Inicio"
-              icon="bi-house-door"
-              currentPath={location.pathname}
+    <header className={clsx('position-fixed d-flex', styles.wrapper)}>
+      <Container className="d-flex py-3">
+        <Menu>
+          <Menu.Header className="d-lg-none">
+            <img
+              src="/src/assets/shield.png"
+              alt="isuarrend-logotipo"
+              className={clsx('w-auto', styles.logo)}
             />
-            <NavLinkItem
-              to="/servicios"
-              label="Servicios"
-              icon="bi-tools"
-              currentPath={location.pathname}
-            />
-          </Nav>
+          </Menu.Header>
 
-          <div className={styles.logoWrapper}>
-            <Link to="/" className={styles.logo}>
-              {/* TODO EMC [06/25/2025]: Add logo */}
-              {/* <img src="/logo.svg" alt="IUSARREND" /> */}
-              IUSARREND
-            </Link>
-          </div>
+          <Menu.Main>
+            <MenuLink to="/" end label="Inicio" />
+            <MenuLink to="/plansPrices" label="Planes y Precios" />
+            <Menu.Header className="d-none d-lg-flex">
+              <img
+                src="/src/assets/shield.png"
+                alt="isuarrend-logotipo"
+                className={clsx('w-auto', styles.logo)}
+              />
+            </Menu.Header>
+            <MenuLink to="/termsConditions" label="Términos y Condiciones" />
+            <MenuLink to="/contact" label="Contacto" />
+          </Menu.Main>
 
-          <Nav className={styles.rightNav}>
-            <NavLinkItem
-              to="/nosotros"
-              label="Nosotros"
-              icon="bi-people"
-              currentPath={location.pathname}
-            />
-            <NavLinkItem
-              to="/contacto"
-              label="Contacto"
-              icon="bi-envelope"
-              currentPath={location.pathname}
-            />
-          </Nav>
-        </Container>
-      </Navbar>
+          <Menu.Footer>Footer</Menu.Footer>
+        </Menu>
+      </Container>
     </header>
   )
 }
