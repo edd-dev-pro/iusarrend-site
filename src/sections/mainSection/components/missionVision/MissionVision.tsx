6a824034903b988857d7fcde9cc0041bfdc3e319
edd.dev <soka_117@live.com>
2025-07-06 20:40:20 -0600

    chore: the texts are updated

--- src/sections/mainSection/components/missionVision/MissionVision.tsx
+++ src/sections/mainSection/components/missionVision/MissionVision.tsx
@@ -1,12 +1,19 @@
 const MissionVision = () => {
   return (
     <div className="limit">
-      <h6>Misión & Visión</h6>
+      <h6>Misión</h6>
+      <p>
+        Ser la empresa líder en México en protección Arrendaria, garantizando
+        seguridad, confianza y respaldo legal para arrendadores, mediante
+        soluciones claras, mediadoras, efectivas y humanas.
+      </p>
+      <h6>Visión</h6>
       <p>
         Proteger los intereses legales de nuestros clientes en contratos de
-        arrendamiento a través de asesoría especializada, redacción de
-        contratos, procesos de investigación y representación jurídica,
-        brindando certeza y tranquilidad durante toda la relación arrendaticia.
+        arrendamiento a través de asesoría especializada, puntual y oportuna,
+        mediante redacción de contratos concretos y transparentes, realizando
+        procesos de investigación y representación legal, brindando certidumbre
+        y tranquilidad durante toda la relación arrendaticia.
       </p>
     </div>
   )
