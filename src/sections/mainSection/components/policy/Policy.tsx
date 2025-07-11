4d60dafbd7e2a6da58907f29adcd1cbf95a4b3ca
edd.dev <soka_117@live.com>
2025-07-11 13:59:14 -0600

    feat: the policy section is created

--- src/sections/mainSection/components/policy/Policy.tsx
+++ src/sections/mainSection/components/policy/Policy.tsx
@@ -1,13 +1,27 @@
+import { Container } from 'react-bootstrap'
+import styles from './styles/policy.module.css'
+import clsx from 'clsx'
+
 const Policy = () => {
   return (
-    <div className="limit">
-      <h6>¿Qué es la Póliza de Arrendamiento?</h6>
-      <p>
-        Ser la póliza jurídica líder en México en materia de arrendamiento,
-        garantizando seguridad, confianza y respaldo legal tanto para
-        arrendadores como para arrendatarios, mediante soluciones claras,
-        efectivas y humanas.
-      </p>
+    <div className={clsx(styles.bg)}>
+      <Container>
+        <div className={clsx('row justify-content-md-center', styles.spacing)}>
+          <div className="col-2 d-none d-md-blok"></div>
+          <div className="col-12 col-md-10 text-center policy">
+            <h2 className={clsx(styles.titleSection)}>
+              Póliza de Arrendamiento
+            </h2>
+            <p className={clsx('mb-0', styles.textSection)}>
+              Ser la póliza jurídica líder en México en materia de
+              arrendamiento, garantizando seguridad, confianza y respaldo legal
+              tanto para arrendadores como para arrendatarios, mediante
+              soluciones claras, efectivas y humanas.
+            </p>
+          </div>
+          <div className="col-2 d-none d-md-blok"></div>
+        </div>
+      </Container>
     </div>
   )
 }
