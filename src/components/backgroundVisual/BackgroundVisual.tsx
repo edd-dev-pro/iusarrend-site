ab235f12c8710c61a24ee7d5ec4d759489c6576c
edd.dev <soka_117@live.com>
2025-07-24 10:00:07 -0600

    feat: the background visual component is deleted

--- src/components/backgroundVisual/BackgroundVisual.tsx
+++
@@ -1,8 +0,0 @@
-import type { FC } from 'react'
-import styles from './styles/backgroundVisual.module.css'
-
-const BackgroundVisual: FC = () => {
-  return <div className={styles.bgImg} />
-}
-
-export default BackgroundVisual
