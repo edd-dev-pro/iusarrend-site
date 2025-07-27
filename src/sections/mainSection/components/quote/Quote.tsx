853285857313466b0ac25f3c9cb90db5c9942d38
edd.dev <soka_117@live.com>
2025-07-26 19:11:44 -0600

    feat: the structure of the quote is updated

--- src/sections/mainSection/components/quote/Quote.tsx
+++ src/sections/mainSection/components/quote/Quote.tsx
@@ -1,7 +1,20 @@
+import { Col, Container, Row } from 'react-bootstrap'
+import styles from './styles/quote.module.css'
+import { QuoteForm } from '../../../../components'
+
 const Quote = () => {
   return (
-    <div className="limit">
-      <h1>Cotizador</h1>
+    <div>
+      <Container>
+        <Row className={styles.spacing}>
+          <Col xs={12}>
+            <h1 className={styles.titleSection}>Cotizador</h1>
+          </Col>
+          <Col xs={12}>
+            <QuoteForm />
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
